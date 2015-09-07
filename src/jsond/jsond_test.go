e766189d1319b2c88ddce220dcf0a98c0ade08ec
jjosephy <jjosephy@hotmail.com>
2015-09-07 09:25:53 -0700

    extended test

--- src/jsond/jsond_test.go
+++ src/jsond/jsond_test.go
@@ -11,8 +11,7 @@ type SimpleJson struct {
     S []string `json:"strings"`
 }
 
-func AssertIsTrue(t *testing.T, c bool, msg string, e error) {
-    t.Logf("c: %v", c)
+func AssertIsTrue(t *testing.T, c bool, msg string, e interface{}) {
     if c != true {
         if e != nil {
             t.Fatalf("Error Msg: %s - Details: %v", msg, e)
@@ -20,6 +19,14 @@ func AssertIsTrue(t *testing.T, c bool, msg string, e error) {
             t.Fatalf("Error Msg: %s", msg)
         }
     }
+}
+
+func Test_Success_CreateJsonNode(t *testing.T) {
+    jRoot, e := Parse(SIMPLE_JSON)
+    AssertIsTrue(t, e == nil, "Failed trying to parse Simple Json String", e)
+
+    jNode := jRoot.Property("strings")
+    AssertIsTrue(t, jNode.Value() != nil, "Value is Nil", jNode)
 }
 
 func Test_Success_IterateSimpleCollection(t *testing.T) {
@@ -35,9 +42,17 @@ func Test_Success_IterateSimpleCollection(t *testing.T) {
     AssertIsTrue(t, propN.Value() != nil, "JsonNode has no value", nil)
 
     v := propN.Iterator();
+
+    p := v.Last()
+    AssertIsTrue(t, p.Value() != nil, "Nil value returned by Last()", nil)
+    AssertIsTrue(t, v.End == true, "End should be true", v.End)
+
+    p = v.First()
+    AssertIsTrue(t, p.Value() != nil, "Nil value returned by First()", nil)
+    AssertIsTrue(t, v.End == false, "End should be false", v.End)
+
     AssertIsTrue(t, v != nil, "Nil Interator on Collection", nil)
     for p := v.Next(); v.End != true; p = v.Next() {
-        j := *p
-        t.Logf("value: %v", j.Value())
+        AssertIsTrue(t, p != nil, "Next returned Nil Pointer", nil)
     }
 }
